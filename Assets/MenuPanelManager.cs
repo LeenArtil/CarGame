7791c968762df02235cdf1afc996fa14545cfc2f
LeenArtil <lma241@usal.edu.lb>
2025-06-22 02:53:30 +0300

    background

--- Assets/MenuPanelManager.cs
+++ Assets/MenuPanelManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 public class MenuPanelManager : MonoBehaviour
@@ -6,9 +6,17 @@ public class MenuPanelManager : MonoBehaviour
     public TextMeshProUGUI bestScoreText;
     void Start()
     {
+       // ResetProgress();
         ScoreManager.Instance?.SetBestScoreTextReference(bestScoreText);
     }
 
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.SetInt("BestScore", 0);
+        PlayerPrefs.Save();
+        Debug.Log("✅ Progress reset. BestScore is now 0.");
+    }
 
     // Update is called once per frame
     void Update()
